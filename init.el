3a172a06d908d50ccfbc49ee9d1ce40141d91a37
Tim McGilchrist <timmcgil@gmail.com>
2011-10-27 11:23:32 +1100

    Added feature-mode for editing cucumber tests and following step definitions.

--- init.el
+++ init.el
@@ -49,10 +49,9 @@
 
 (require 'el-get)
 
-
 (setq my-packages
       (append
-       '(el-get nxhtml)
+       '(el-get nxhtml feature-mode)
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get 'sync my-packages)
