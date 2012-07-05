d89d25758a7f32fab1b372994dd5199db69d3a5f
Tim McGilchrist <timmcgil@gmail.com>
2012-07-06 09:59:04 +1000

    Change vc-diff colours.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(ecb-auto-activate t)
  '(ecb-layout-name "left15")
  '(ecb-options-version "2.40")
- '(ecb-source-path (quote ("/Users/tim/")))
+ '(ecb-source-path (quote ("/Users/tim/" ("/Users/tim/Blake/re" "Reading Eggs") ("/Users/tim/Blake/rex-student" "Reading Eggspress") ("/Users/tim/Blake/blake-admin" "Admin"))))
  '(ecb-tip-of-the-day nil))
 
 (custom-set-faces
@@ -14,6 +14,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:foreground "Green"))) t)
+ '(diff-removed ((t (:foreground "Red"))) t)
  '(ecb-default-highlight-face ((t (:background "dark olive green"))))
  '(mumamo-background-chunk-major ((t nil)))
  '(mumamo-background-chunk-submode1 ((t (:background "#1A2E00")))))
