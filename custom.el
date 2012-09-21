af24532662265838b8ef32eaed3ad1d72b04a785
Tim McGilchrist <timmcgil@gmail.com>
2012-09-21 14:42:39 +1000

    format changed.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(ecb-auto-activate t)
  '(ecb-layout-name "left14")
  '(ecb-options-version "2.40")
- '(ecb-source-path (quote ("/Users/tim/")))
+ '(ecb-source-path (quote ("/Users/tim/" ("/Users/tim/Blake/re" "Reading Eggs") ("/Users/tim/Blake/rex-student" "REX") ("/Users/tim/Blake/blake-admin" "Admin") ("/Users/tim/Blake/blake-shared" "blake-shared") ("/Users/tim/Blake/blake-common" "blake-common") "/Users/tim/Blake/blake-cloud")))
  '(ecb-tip-of-the-day nil))
 
 (custom-set-faces
@@ -14,8 +14,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diff-added ((t (:foreground "Green"))) t)
- '(diff-removed ((t (:foreground "Red"))) t)
+ '(diff-added ((t (:foreground "Green"))))
+ '(diff-removed ((t (:foreground "Red"))))
  '(ecb-default-highlight-face ((t (:background "dark olive green"))))
  '(mumamo-background-chunk-major ((t nil)))
  '(mumamo-background-chunk-submode1 ((t (:background "#1A2E00")))))
