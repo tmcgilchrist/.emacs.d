6b9f3fad4ae15c2183dd80cb67d373cfc3085e38
Tim McGilchrist <timmcgil@gmail.com>
2013-05-20 15:26:36 +1000

    Disable various rspec-mode options to work with Zeus.

--- tim/ruby-setup.el
+++ tim/ruby-setup.el
@@ -21,6 +21,12 @@
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
 
+
+(setq rspec-use-rake-flag nil)
+(setq rspec-use-rvm t)
+(setq rspec-use-bundler-when-possible nil)
+(setq rspec-use-opts-file-when-available t)
+
 (defadvice rspec-compile (around rspec-compile-around)
   "Use BASH shell for running the specs because of ZSH issues."
   (let ((shell-file-name "/bin/bash"))
