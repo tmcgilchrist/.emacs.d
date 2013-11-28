6631dcf659aa1b0aa470014a20f85f8bfc5d903a
Tim McGilchrist <timmcgil@gmail.com>
2013-11-29 10:04:42 +1100

    Custom searching for work projects.

--- tim/ruby-setup.el
+++ tim/ruby-setup.el
@@ -1,6 +1,8 @@
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook 'ruby-electric-mode)
 (add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)
+(add-hook 'ruby-mode-hook 'paredit-mode)
+(add-hook 'ruby-mode-hook 'rinari-minor-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\.haml$" . haml-mode))
@@ -18,13 +20,15 @@
             (define-key ruby-mode-map "\C-c\C-c"
                               'comment-or-uncomment-region)))
 
+(add-hook 'after-init-hook 'inf-ruby-switch-setup)
+
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
 
-
+(setq rspec-use-rake-when-possible nil)
 (setq rspec-use-rake-flag nil)
 (setq rspec-use-rvm t)
-(setq rspec-use-bundler-when-possible nil)
+(setq rspec-use-bundler-when-possible t)
 (setq rspec-use-opts-file-when-available t)
 
 (defadvice rspec-compile (around rspec-compile-around)
@@ -42,3 +46,48 @@
   (let ((shell-file-name "/bin/bash"))
     ad-do-it))
 (ad-activate 'feature-run-cucumber)
+
+(defun blake-rgrep (&optional arg)
+  "Based on rinari..."
+  (interactive "P")
+  (grep-compute-defaults)
+  (if arg (call-interactively 'rgrep)
+    (let ((query))
+      (if mark-active
+          (setq query (buffer-substring-no-properties (point) (mark)))
+        (setq query (thing-at-point 'ruby-word)))
+      (funcall 'rgrep
+               (read-from-minibuffer "search for: " query)
+               "*.rb *.haml *.rhtml *.erb *.coffee *.rake *.sass *.scss Gemfile Gemfile.lock *.emblem *.yml"
+               "~/Blake"))))
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Completing point by some yasnippet key
+(defun yas-ido-expand ()
+  "Lets you select (and expand) a yasnippet key"
+  (interactive)
+    (let ((original-point (point)))
+      (while (and
+              (not (= (point) (point-min) ))
+              (not
+               (string-match "[[:space:]\n]" (char-to-string (char-before)))))
+        (backward-word 1))
+    (let* ((init-word (point))
+           (word (buffer-substring init-word original-point))
+           (list (yas-active-keys)))
+      (goto-char original-point)
+      (let ((key (remove-if-not
+                  (lambda (s) (string-match (concat "^" word) s)) list)))
+        (if (= (length key) 1)
+            (setq key (pop key))
+          (setq key (ido-completing-read "key: " list nil nil word)))
+        (delete-char (- init-word original-point))
+        (insert key)
+        (yas-expand)))))
+
+(define-key yas-minor-mode-map (kbd "<C-tab>")     'yas-ido-expand)
+
+(global-set-key (kbd "C-c , b") 'blake-rgrep)
+(global-set-key (kbd "C-c ' b") 'blake-rgrep)
