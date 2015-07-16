5fb42dc6084964c228f33a697833218346f69d44
Tim McGilchrist <timmcgil@gmail.com>
2015-07-16 14:05:38 +1000

    Key bindings for haskell mode.

--- tim/haskell.el
+++ tim/haskell.el
@@ -5,6 +5,8 @@
             (define-key haskell-mode-map "\C-c\C-c"
                               'comment-or-uncomment-region)))
 
+(custom-set-variables
+  '(haskell-tags-on-save t))
 
 (eval-after-load "haskell-mode"
   ;; Replace the haskell-mode version with this version,
@@ -31,3 +33,12 @@
                           padding
                           line)
                " ")))
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
